527f0300f86590f01bebfb643aa6020e72b8d90a
A1-mamun <1154almamun@gmail.com>
2024-09-09 21:12:48 +0600

    Authprovider navbar authentication page created

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { router } from "./routes/Routes";
+import { Toaster } from "react-hot-toast";
+import { HelmetProvider } from "react-helmet-async";
+import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./providers/AuthProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </AuthProvider>
+    </HelmetProvider>
   </StrictMode>
 );
