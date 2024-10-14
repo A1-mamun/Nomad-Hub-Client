15fab33a1f3f44bb32da2fdc8b47ddea6d63484a
A1-mamun <1154almamun@gmail.com>
2024-10-14 16:08:09 +0600

    Statistics page complete for all role

--- src/pages/Dashboard/Common/Statistics.jsx
+++ src/pages/Dashboard/Common/Statistics.jsx
@@ -1,12 +1,20 @@
 import useRole from "../../../hooks/useRole";
 import LoadingSpinner from "../../../components/Shared/LoadingSpinner";
 import AdminStatistics from "../Admin/AdminStatistics";
+import HostStatistics from "../Host/HostStatistics";
+import GuestStatistics from "../Guest/GuestStatistics";
 
 const Statistics = () => {
   const [role, isLoading] = useRole();
 
   if (isLoading) return <LoadingSpinner />;
-  return <>{role === "Admin" && <AdminStatistics />}</>;
+  return (
+    <>
+      {role === "Admin" && <AdminStatistics />}
+      {role === "Host" && <HostStatistics />}
+      {role === "Guest" && <GuestStatistics />}
+    </>
+  );
 };
 
 export default Statistics;
